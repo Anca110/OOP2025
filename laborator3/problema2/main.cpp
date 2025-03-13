8b46a4d0eb1fd10f1897497111296674ff14efa0
Ilenei Ancuta Bianca <ancutailenei55@gmail.com>
2025-03-13 20:38:38 +0200

    problema2, lab3

---
+++ laborator3/problema2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Canvas.h"
+
+int main() {
+    // Creez o matrice de 20x10
+    Canvas myCanvas(20, 10);
+
+    myCanvas.DrawRect(2, 2, 15, 7, '#');
+
+    myCanvas.FillRect(4, 3, 13, 6, '*');
+
+    myCanvas.DrawCircle(10, 5, 3, 'O');
+
+    myCanvas.FillCircle(17, 5, 2, '@');
+
+    myCanvas.DrawLine(1, 1, 18, 8, '+');
+
+    myCanvas.SetPoint(5, 5, '@');
+
+    myCanvas.Print();
+
+    return 0;
+}
