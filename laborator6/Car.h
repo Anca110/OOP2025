3f7463c837d40babca3f57244d9cd0b155ece8bf
Anca110 <ancutailenei55@gmail.com>
2025-04-07 17:53:20 +0300

    Update Car.h

--- laborator6/Car.h
+++ laborator6/Car.h
@@ -1,6 +1,5 @@
 // Car.h
 #pragma once
-#include <string>
 #include "Weather.h"
 using namespace std;
 
@@ -10,11 +9,11 @@ protected://pun la protected fiind clasa de baza(vreau clasele derivate sa le po
     //( nu vreu ca alte clase din AFARA IERARHIEI sa le aceseze=> nu pui public)
     float fuelCapacity;   
     float fuelConsumption; 
-    float speed[3];// viteze în func?ie de vreme
+    float speed[3];// viteze Ã®n func?ie de vreme
     const char* name;//pointer catre un sir pe care NU L POT MODIFICA
 
 public:
-    virtual ~Car() = default; // destructor virtual — important pentru polimorfism!
+    virtual ~Car() = default; // destructor virtual â€” important pentru polimorfism!
 
     virtual float GetSpeed(Weather w) const = 0;//pt ca le fac virtuale(sa le pot accesa diferit pt fiecare obiect din clase derivate de la Car)
     virtual float GetFuelCapacity() const = 0;//virtual pt a putea pune in celelalte clase functii cu acelasi numa; si cand apelez sa mi apeleze pentru aia din clasa derivata unu pentru asta din clasa de baza
@@ -26,4 +25,4 @@ public:
 
 
 
-//!!!!char name[100], ai fi alocat fix 100, mereu. Dar cu char* po?i aloca cât vrei, dinamic.
+//!!!!char name[100], ai fi alocat fix 100, mereu. Dar cu char* po?i aloca cÃ¢t vrei, dinamic.
